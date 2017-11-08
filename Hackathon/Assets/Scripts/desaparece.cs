fea582c247142114183e3532a63640135f5eeeb4
Juan Regules <A00397463@itesm.mx>
2017-11-08 12:50:39 -0600

    descripcion fog
    hice un cubo rectangular que ocupa la mayor parte de la camara, lo cubri con muchos planes de color negro y puse una esfera.
    Tanto a la esfera como a los planos les puse collider y RigidBody
    Le puse un codigo a la esfera para que se pueda mover por la pantalla en dos dimenciones con WASD.
    le puse un codigo a todos los planos que especifica que si son tocados por algun objeto, en este caso la esfera, desaparecen.

---
+++ Hackathon/Assets/Scripts/desaparece.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class desaparece : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(){
+		Destroy (this.gameObject);
+
+
+
+	}
+}
