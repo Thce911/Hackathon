0787c09b14a565f8ee4bf44dc8847e5a412e534f
Juan Regules <A00397463@itesm.mx>
2017-11-08 11:58:23 -0600

    fog
    agregue escena de fog og war con controlador WASD

---
+++ Hackathon/Assets/Scripts/WASD.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASD : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//; punto y coma esta junto a la Ñ
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)){
+			transform.Translate(0,2f,0);
+		}
+		if (Input.GetKey (KeyCode.S)){
+			transform.Translate(0,-2f,0);
+		}
+		if (Input.GetKey (KeyCode.A)){
+			transform.Translate(-2f,0,0);
+		}
+		if (Input.GetKey (KeyCode.D)){
+			transform.Translate(2f,0,0);
+		}
+
+	}
+
+	//; punto y coma esta junto a la Ñ
+
+
+}
