72b7194d859b8cdd0231c5b46479f1a6f5e8e71b
agustin-itesm <agustin.hdm@itesm.mx>
2017-11-13 17:06:24 -0600

    Se quedan fog of war en la memoria
    El fog of war ya descubierto se queda en la memoria

---
+++ Hackathon/Assets/Scripts/FogInitialize.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogInitialize : MonoBehaviour {
+
+	public static List<string> FogTiles = new List<string>(); //variable para guardar lista de tiles
+
+	// Use this for initialization
+	void Start () {
+
+		FogTiles.Clear (); //limpiar lista dinámica de tiles para evitar redundancia
+		if (PlayerPrefs.GetString ("fogtiles") == "") { //si no hay variable fogtiles en la memoria
+			//no hacer nada
+		}  else { //si hay variable fogtiles en la memoria
+			//separar el string de la memoria y convertirlo en la lista de objetos
+			foreach (string objetos in PlayerPrefs.GetString("fogtiles").Split(',')) {
+				FogTiles.Add (objetos);
+			}
+		}
+
+		foreach (Transform child in transform) {
+			if (FogTiles.Contains (child.name)) { //si el tile está en la colección
+				child.gameObject.SetActive (false); //desactivar tile
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Hackathon/Assets/Scripts/WASD.cs
+++ Hackathon/Assets/Scripts/WASD.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WASD : MonoBehaviour {
+	bool arribaBool;
+	bool abajoBool;
 
+	bool izquierdaBool;
+	bool derechaBool;
 	// Use this for initialization
 	void Start () {
+		arribaBool = false;
+		abajoBool = false;
+		izquierdaBool = false;
+		derechaBool = false;
 		//; punto y coma esta junto a la Ñ
 	}
 	
@@ -24,6 +32,21 @@ public class WASD : MonoBehaviour {
 			transform.Translate(-2f,0,0);
 		}
 
+
+
+		if (arribaBool == true) {
+			transform.Translate(0,0,2f);
+		}
+		if (abajoBool == true) {
+			transform.Translate(0,0,-2f);
+		}
+		if (izquierdaBool == true) {
+			transform.Translate(2f,0,0);
+		}
+		if (derechaBool == true) {
+			transform.Translate (-2f, 0, 0);
+		}
+
 	}
 
 	//; punto y coma esta junto a la Ñ
@@ -32,9 +55,47 @@ public class WASD : MonoBehaviour {
 		MeshRenderer meshr;
 		Light lighft;
 		meshr=other.GetComponent<MeshRenderer> ();
-		meshr.enabled = true;
+		meshr.enabled = false;
+
+		if (FogInitialize.FogTiles.Contains (other.name)) { //si el objeto ya están la lista de tiles
+			//no hacer nada
+		}  else { //si el objeto no están la lista de tiles
+			FogInitialize.FogTiles.Add (other.name); //agregar tile a la colección
+			string[] stringArray = FogInitialize.FogTiles.ToArray (); //convertir lista de tiles en array
+			PlayerPrefs.SetString ("fogtiles", string.Join (",", stringArray)); //agregar a la memoria los tiles en 1 string a partir del array
+		}
+	}
+
+	public void arribaOprime(){
+		arribaBool = true;
+	}
+	public void arrbaSuelta(){
+		arribaBool = false;
+	
+	}
+
+
+	public void abajoOprime(){
+		abajoBool = true;
+	}
+	public void abajoSuelta(){
+		abajoBool = false;
 
 	}
 
+	public void izquierdaOprime(){
+		izquierdaBool = true;
+	}
+	public void izquierdaSuelta(){
+		izquierdaBool = false;
+
+	}
+	public void derechaOprime(){
+		derechaBool = true;
+	}
+	public void derechaSuelta(){
+		derechaBool = false;
+
+	}
 
 }
