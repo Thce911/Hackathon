03140e07b9b7ef612fdd9c40a3b1b121023f9b95
Juan Regules <A00397463@itesm.mx>
2017-11-13 14:51:02 -0600

    FOG
    FOG

--- Hackathon/Assets/Scripts/WASD.cs
+++ Hackathon/Assets/Scripts/WASD.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WASD : MonoBehaviour {
+	bool arribaBool;
+	bool abajoBool;
 
+	bool izquierdaBool;
+	bool derechaBool;
 	// Use this for initialization
 	void Start () {
+		arribaBool = false;
+		abajoBool = false;
+		izquierdaBool = false;
+		derechaBool = false;
 		//; punto y coma esta junto a la Ñ
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.W)){
+		/* if (Input.GetKey (KeyCode.W)){
 			transform.Translate(0,0,2f);
 		}
 		if (Input.GetKey (KeyCode.S)){
@@ -24,6 +32,21 @@ public class WASD : MonoBehaviour {
 			transform.Translate(-2f,0,0);
 		}
 
+
+
+		if (arribaBool == true) {
+			transform.Translate(0,0,2f);
+		}
+		if (abajoBool == true) {
+			transform.Translate(0,0,-2f);
+		}
+		if (izquierdaBool == true) {
+			transform.Translate(2f,0,0);
+		}
+		if (derechaBool == true) {
+			transform.Translate (-2f, 0, 0);
+		}*/
+
 	}
 
 	//; punto y coma esta junto a la Ñ
@@ -32,9 +55,40 @@ public class WASD : MonoBehaviour {
 		MeshRenderer meshr;
 		Light lighft;
 		meshr=other.GetComponent<MeshRenderer> ();
-		meshr.enabled = true;
+		meshr.enabled = false;
+
+	}
+	/*
+	public void arribaOprime(){
+		arribaBool = true;
+	}
+	public void arrbaSuelta(){
+		arribaBool = false;
+	
+	}
+
+
+	public void abajoOprime(){
+		abajoBool = true;
+	}
+	public void abajoSuelta(){
+		abajoBool = false;
 
 	}
 
+	public void izquierdaOprime(){
+		izquierdaBool = true;
+	}
+	public void izquierdaSuelta(){
+		izquierdaBool = false;
+
+	}
+	public void derechaOprime(){
+		derechaBool = true;
+	}
+	public void derechaSuelta(){
+		derechaBool = false;
+
+	} */
 
 }
