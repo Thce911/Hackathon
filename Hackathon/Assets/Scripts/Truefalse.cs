28caab945256d94cf4b722d7be24822c63548d9c
Juan Regules <A00397463@itesm.mx>
2017-11-14 16:44:47 -0600

    act 1 y 2
    act 1 y 2

---
+++ Hackathon/Assets/Scripts/Truefalse.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Truefalse : MonoBehaviour {
+	
+	public Image trueimage;
+	public Image falseimage;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void VERDADERO(){
+		//trueimage.enabled = true;
+		trueimage.enabled=true;
+		falseimage.enabled = false;
+	
+	}
+	public void FALSO(){
+		//falseimage.enabled = true;
+		falseimage.enabled=true;
+		trueimage.enabled = false;
+	
+	}
+}
